af67e7baca2b9e9074398bb7eed3ff9ca28da809
Danniely Santos <dannielyingrid@gmail.com>
2020-07-30 18:03:23 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -3,7 +3,7 @@ def parrot (phrase = "Squawk!")
   return "That's the sound I make"
 end
 
-parrot
+parrot ("Bird")
 
   # Create method `parrot` that outputs a given phrase and
 # returns the phrase
